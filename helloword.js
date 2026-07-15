c07cd3d14e093e66bb07727bb678ca1602e9b055
Shlok kumar <shlok200417@gmail.com>
2026-07-15 23:05:59 +0530

    Introduce global variable and function to log its value
    Added a global variable 'Grade' and demonstrated its usage within a function.

--- helloword.js
+++ helloword.js
@@ -4,3 +4,17 @@ document.write('Hello, World!');
 console.log('Hello, World!');
 //program to print Hello, World!  
 alert('Hello, World!');  
+var Grade = "B";  
+// Declaring global variable outside the function  
+myFunction();  
+// Global variable accessed from   
+// Within a function  
+function myFunction() {  
+    console.log("global value of Grade is: ", Grade);  
+}  
+// Changing value of global  
+// Variable from outside of function  
+{  
+    Grade = "A";  
+    console.log("local value of Grade is: ", Grade);  
+}  
