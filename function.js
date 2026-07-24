ff3ad7178eebc6da6670d937c1f241a559f7aff2
Shlok kumar <shlok200417@gmail.com>
2026-07-24 21:31:42 +0530

    Add functions to demonstrate local variable scope
    Added two new functions, mydemo1 and mydemo2, to demonstrate local variable scope. Updated console log statements to reflect the changes.

--- function.js
+++ function.js
@@ -6,4 +6,19 @@ function myfunction(){
     console.log(word);  
 }  
   
-console.log(word);    
+console.log(word);  
+
+mydemo1();  
+mydemo2();  
+let word;  
+function mydemo1(){  
+    //Local variable  
+    let word = "Tpoint Tech";  
+    console.log(word);  
+}  
+function mydemo2(){  
+    //Local variable  
+    let word = "Tpoint";  
+    console.log(word);  
+}  
+console.log(word);  
