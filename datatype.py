1005d0b9d2bac26663277de4599d04c553288040
Shlok kumar <shlok200417@gmail.com>
2026-07-29 00:01:33 +0530

    Add string variables and template literals to datatype.py

--- datatype.py
+++ datatype.py
@@ -6,3 +6,9 @@ let c = Infinity;
 console.log(c)  
 let d = 'something here too' / 2;  
 console.log(d)   
+let a = "TpointTech";  
+console.log(a);   
+let b = 'Single quotes work fine';  
+console.log(b);   
+let c = `can embed ${a}`;  
+console.log(c);  
