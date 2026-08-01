069e217c7076c5c333620997d0bfd385258b9a8c
Shlok kumar <shlok200417@gmail.com>
2026-08-01 08:05:25 +0530

    Correct conditional logic and syntax in conditional.js
    Fix syntax errors and improve conditional statements.

--- conditional.js
+++ conditional.js
@@ -10,3 +10,18 @@ document.write("the value of a is even");
 else {
   document.write("the value of a is odd);
      }
+//JavaScript If...else if statement
+
+var = 20 ;
+if (a==10){
+  document.write("the value a is equal to 10");
+}
+elseif(a==15){
+document.wirte("the value a is equal to 15");
+}
+elseif(a==20){
+document.write("the value a is equal to 20");
+}
+else{
+  document.wirte("the value a is not equal to 10,15,20);
+                 }
