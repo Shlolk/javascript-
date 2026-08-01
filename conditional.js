10c846ca024cf9b07e133ab4fdc887e09e91f8ec
Shlok kumar <shlok200417@gmail.com>
2026-08-01 07:59:53 +0530

    Correct conditional logic and syntax in conditional.js
    Fix syntax errors in conditional statements and improve output messages.

--- conditional.js
+++ conditional.js
@@ -3,3 +3,10 @@ var a=20;
 if(a>10){  
 document.write("value of a is greater than 10");  
 }  
+var = 20 ;
+if (a%20=0){
+document.write("the value of a is even");
+}
+else {
+  document.write("the value of a is odd);
+                 }
